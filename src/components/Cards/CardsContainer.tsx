bdeee1cd5b7409f522d8f40ab64578f91d771199
Kirill Medved <medved.kirill@bk.ru>
2021-05-02 23:23:25 +0300

    Added Card

--- src/components/Cards/CardsContainer.tsx
+++ src/components/Cards/CardsContainer.tsx
@@ -1,7 +1,45 @@
 import React from 'react';
+import TinderCard from 'react-tinder-card';
+import cards from './CardsContainer.module.css';
+import { IUser } from '../../../interfaces/IUser';
 
-const CardsContainer = () => {
-  return <div>Cards</div>;
+interface ICardsContainerProps {
+  people: IUser[];
+  swiped: (direction: string, nameToDelete: string) => void;
+  outOfFrame: (name: string) => void;
+  childRefs: any;
+}
+
+const CardsContainer: React.FC<ICardsContainerProps> = ({
+  people,
+  swiped,
+  outOfFrame,
+  childRefs,
+}) => {
+  return (
+    <>
+      <div className={cards.cardsContainer}>
+        {people.map((person, index) => (
+          <div className={cards.cardContainer} key={person.id}>
+            <TinderCard
+              ref={childRefs[index]}
+              preventSwipe={['down']}
+              onSwipe={(dir) => swiped(dir, person.name)}
+              onCardLeftScreen={() => outOfFrame(person.name)}
+            >
+              <div
+                style={{ backgroundImage: `url(${person.photos})` }}
+                className={cards.card}
+              >
+                <h3>{person.name}</h3>
+                <h4>{person.title ? person.title : ''}</h4>
+              </div>
+            </TinderCard>
+          </div>
+        ))}
+      </div>
+    </>
+  );
 };
 
 export default CardsContainer;
