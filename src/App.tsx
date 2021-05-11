b67d6baa9288e6bac6f4cc331d94761c470b1464
Kirill Medved <medved.kirill@bk.ru>
2021-05-11 22:21:05 +0300

    Update Routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,30 @@
 import React from 'react';
 import { Route, BrowserRouter } from 'react-router-dom';
+import { Redirect } from 'react-router';
+
 import ProfileContainer from './components/Profile';
 import Login from './components/Login';
 import './App.css';
+import Register from './components/Register';
 
 function App() {
   return (
     <BrowserRouter basename={process.env.PUBLIC_URL}>
-      <div className='app-wrapper'>
-        <Route path='/profile' render={() => <ProfileContainer />} />
-        {/* <Route path='/dialogs' render={() => <DialogsContainer />} /> */}
-      </div>
-      <Route path='/' render={() => <Login />} />
+      <Route path='/' exact render={() => <Redirect to='login' />} />
+      <Route path='/login' render={() => <Login />} />
+      <Route path='/register' render={() => <Register />} />
+      <Route
+        path='/profile'
+        render={() => {
+          return (
+            <div className='app-wrapper'>
+              <ProfileContainer />
+            </div>
+          );
+        }}
+      />
+
+      {/* <Route path='/dialogs' render={() => <DialogsContainer />} /> */}
     </BrowserRouter>
   );
 }
